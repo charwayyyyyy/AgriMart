22afdda707da1ff4f8b1b26ed049e5804eb66aa9
Theophilus David Nana Yaw Kyei-Charway <theochar0553634532@gmail.com>
2025-08-07 14:10:25 +0000

    Add required dev dependencies for babel and polyfills

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
 /** @type {import('next').NextConfig} */
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 const nextConfig = {
   /* config options here */
+  // Increase memory limit for webpack
+  experimental: {
+    webpackBuildWorker: true,
+    memoryBasedWorkersCount: true
+  },
+  webpack: (config, { isServer }) => {
+    // Only apply polyfills in the browser build
+    if (!isServer) {
+      // Add polyfills for node modules
+      config.plugins.push(new NodePolyfillPlugin());
+      
+      // Exclude problematic modules from client bundle
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        undici: false,
+        'node-fetch': false,
+      };
+    }
+    
+    return config;
+  }
 };
 
 module.exports = nextConfig;
